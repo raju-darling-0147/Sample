044d8294097c6280c4c544060d0f19fcb9fc0cdf
raju-darling-0147 <50623117+raju-darling-0147@users.noreply.github.com>
2019-05-14 17:32:02 +0530

    Add files via upload

---
+++ app4/WebContent/WEB-INF/loginform.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>Durga Software Solutions</h2>
+<h3>User Login page</h3>
+<form method="POST" action="login">
+<table>
+<tr>
+<td>User Name</td>
+<td><input type="text" name="uname"/></td>
+</tr>
+<tr>
+<td>User Password</td>
+<td><input type="password" name="upwd"/></td>
+</tr>
+<tr>
+<td><input type="submit" value="Login"/></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
---
+++ app4/src/com/durgasoft/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package com.durgasoft.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class LoginController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String uname=request.getParameter("uname");
+		String upwd=request.getParameter("upwd");
+		
+		ModelAndView mav=null;
+		if(uname.equals("durga")&& upwd.equals("durga")) {
+			mav=new ModelAndView("success");
+		}else {
+			mav=new ModelAndView("failure");
+		}
+		return mav;
+	}
+
+}
